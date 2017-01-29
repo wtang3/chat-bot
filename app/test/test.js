5f5554688c457da8d15ce777ea52d61374d6055d
William Tang <wtang3@outlook.com>
2017-01-29 01:30:15 -0500

    initial commit

---
+++ app/test/test.js
@@ -0,0 +1,9 @@
+var assert = require('chai').assert,
+    expect = require('chai').expect;
+
+
+describe('Initial test', function(){
+    it('1+1 should be 2', function(){
+        assert.equal(1+1, 2, 'The answer should be 2')
+    });
+});
