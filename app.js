2017a736a5a10572cbe122cc5c1c506666457a95
wtang <wtang3@outlook.com>
2017-02-03 18:13:49 -0500

    Refactored root and added ping endpoint

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 var express = require('express');
     app = express(),
     scv = require('./app/scv.js');
-
+    path = __dirname + '/app/dist/';
 
 var port = 3000;
 
+app.use(express.static(__dirname + '/app/dist'));
+
 // Main page
 app.get('/', function(req, res){
+    try{
+        res.sendFile(path + "index.html");
+    }catch(e){
+        res.send("Uh oh 4xx");
+    }
+
+});
+
+app.get('/ping', function(req, res){
     try{
         var message = scv.hello();
         res.send(message);
     }catch(e){
         res.send("Uh oh SCV died for some reason.");
     }
-});
+})
 
 app.listen(port,function(){
     console.log('App started on port ' + port);
