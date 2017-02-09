48a5a76eb0cb66a4c419e85a3ee2d53b4a02f799
wtang <wtang3@outlook.com>
2017-02-08 20:29:06 -0500

    added config file for strings

---
+++ config.js
@@ -0,0 +1,14 @@
+var config = {};
+
+// Hello SCV
+config.scv = {
+    ping   : "What's going on? SCV ready.",
+    error  : "Uh oh SCV died for some reason.",
+    error2 : "Uh oh you did something wrong."
+};
+
+config.facebook = {
+    verificationError : "Failed verification"
+};
+
+module.exports = config;
